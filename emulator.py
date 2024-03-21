802ef3d818d3f84b8d17418f8a93d3787890397c
rauf8888 <mrauf2907@gmail.com>
2024-03-21 23:11:33 +0530

    File Modified: Added Initial Packages

--- emulator.py
+++ emulator.py
@@ -1,4 +1,9 @@
-from bs4 import BeautifulStoneSoup
+from colorama import Fore,Style
+from bs4 import BeautifulSoup
+from pprint import pprint
+from tqdm import tqdm
+import pandas as pd
 import requests
 
-site = requests.get("")
+site = requests.get("https://1337xto.to")
+pprint(site)
